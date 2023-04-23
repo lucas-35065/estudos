ada7150682e96f9fc3c103659b7b6d5f726c36be
LucasRodrigues <lucas.rodrigues.35065@gmail.com>
2023-04-23 20:03:14 -0300

    form de cadastro concluído, indo para cadastro de prods

--- Formularios/js/validacao.js
+++ Formularios/js/validacao.js
@@ -39,13 +39,28 @@ const mensagensDeErro = {
     },
     cpf: {
         valueMissing: 'O campo de CPF não pode estar vazio.',
-        customError: 'O Cpf digitado não é válido.'
+        customError: 'O CPF digitado não é válido.'
+    },
+    cep: {
+        valueMissing: 'O campo de CEP não pode estar vazio.',
+        patternMismatch: 'O CEP informado não é válido.',
+        customError: 'Não foi possível buscar o CEP.'
+    },
+    logradouro: {
+        valueMissing: 'O campo de Logradouro não pode estar vazio.'
+    },
+    cidade: {
+        valueMissing: 'O campo de Cidade não pode estar vazio.'
+    },
+    estado: {
+        valueMissing: 'O campo de Estado não pode estar vazio.'
     },
 }
 
 const validadores = {
     dataNascimento: input => validaDataNascimento(input),
-    cpf: input => validaCPF (input)
+    cpf: input => validaCPF (input),
+    cep: input => recuperarCep (input),
 
 }
 
@@ -81,7 +96,7 @@ function validaCPF (input) {
     const cpfFormatado = input.value.replace(/\D/g,'')
     let mensagem = '';
 
-    if (!checaCPFRepetido (cpfFormatado)){
+    if (!checaCPFRepetido (cpfFormatado) || !checaEstruturaCPF(cpfFormatado)){
         mensagem = 'O Cpf digitado não é válido.'
     }
 
@@ -112,5 +127,68 @@ function checaCPFRepetido (cpf) {
 }
 
 function checaEstruturaCPF (cpf) {
+    const multiplicador = 10
+
+    return checaDigitoVerificador(cpf, multiplicador)
+}
+
+function checaDigitoVerificador (cpf, multiplicador) {
+    if (multiplicador >= 12){
+        return true
+    }
     
+    let multiplicadorInicial = multiplicador
+    let soma = 0
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('')
+    const digitoVerificador = cpf.charAt(multiplicador - 1)
+    for (let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--){
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+        contador++
+    }
+    if (digitoVerificador == confirmaDigito(soma)) {
+        return checaDigitoVerificador (cpf, multiplicador + 1)
+    }
+
+    return false
+}
+function confirmaDigito(soma) {
+    return 11 - (soma % 11)
+}
+
+function recuperarCep (input){
+    const cep = input.value.replace(/\D/g, '')
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'content-type': 'application/json;charset=utf-8'
+        },
+    }
+
+    if (!input.validity.patternMismatch && !input.validity.valueMissing){
+        fetch (url, options).then(
+            response => response.json()
+        ).then (
+            data => {
+                if(data.erro) {
+                    input.setCustomValidity('Não foi possível buscar o CEP.')
+                    return
+                }
+                input.setCustomValidity('')
+                preencheCamposComCEP(data)
+                return
+            }
+        )
+    }
+}
+
+function preencheCamposComCEP(data) {
+    const logradouro = document.querySelector ('[data-tipo="logradouro"]')
+    const cidade = document.querySelector ('[data-tipo="cidade"]')
+    const estado = document.querySelector ('[data-tipo="estado"]')
+
+    logradouro.value = data.logradouro
+    cidade.value = data.localidade
+    estado.value = data.uf
 }
