f9940f29eab042496d308b01174bdb18634c8c45
LucasRodrigues <lucas.rodrigues.35065@gmail.com>
2023-05-10 20:35:38 -0300

    adicionando mais coisas de sql

---
+++ sql/mistura maravilhosa de funções.sql
@@ -0,0 +1,12 @@
+use sucos_vendas;
+
+SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+
+SELECT CONCAT('O cliente ', TC.NOME, ' faturou ', 
+CAST(SUM(INF.QUANTIDADE * INF.preco) AS char (20))
+ , ' no ano ', CAST(YEAR(NF.DATA_VENDA) AS char (20))) AS SENTENCA FROM notas_fiscais NF
+INNER JOIN itens_notas_fiscais INF ON NF.NUMERO = INF.NUMERO
+INNER JOIN tabela_de_clientes TC ON NF.CPF = TC.CPF
+WHERE YEAR(DATA_VENDA) = 2016
+GROUP BY TC.NOME, YEAR(DATA_VENDA);
+
