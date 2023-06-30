48528d1be8f97f8ad6b40c71fbe69019c8aa3426
LucasRodrigues <lucas.rodrigues.35065@gmail.com>
2023-06-29 22:03:26 -0300

    Atualização do CSS das páginas

--- aluroni/src/pages/Cardapio/Buscador/index.tsx
+++ aluroni/src/pages/Cardapio/Buscador/index.tsx
@@ -1,24 +1,21 @@
 import React from "react";
-import styles from "./Buscador.module.scss"
+import styles from "./Buscador.module.scss";
 import { CgSearch } from "react-icons/cg";
 
-interface Props{
-    busca: string,
-    setBusca: React.Dispatch<React.SetStateAction<string>>
+interface Props {
+  busca: string;
+  setBusca: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function Buscador ({busca, setBusca}: Props) {
-    return(
-        <div className={styles.buscador}>
-            <input 
-                value={busca} 
-                onChange={(evento) => setBusca(evento.target.value)}
-                placeholder="Qual é sua fome hoje?"
-            />
-            <CgSearch 
-            size={20}
-            color="#4C4D5E"
-            />
-        </div>
-    )
+export default function Buscador({ busca, setBusca }: Props) {
+  return (
+    <div className={styles.buscador}>
+      <input
+        value={busca}
+        onChange={(evento) => setBusca(evento.target.value)}
+        placeholder="Qual é a sua fome de hoje?"
+      />
+      <CgSearch size={20} color="#4C4D5E" />
+    </div>
+  );
 }
