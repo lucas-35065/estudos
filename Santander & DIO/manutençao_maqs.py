68d9156be68ab0a96a50da0ebd9db9f362d3bde8
Lucas Rodrigues <lucas.rodrigues.35065@gmail.com>
2023-10-13 19:00:40 +0000

    Leitura do df e conversão em lista

--- Santander & DIO/manutençao_maqs.py
+++ Santander & DIO/manutençao_maqs.py
@@ -1,24 +1,25 @@
+import json
 import pandas as pd
 import requests
-import json
 
+
+# Leitura do arquivo.csv
 df = pd.read_csv('manut_maqs_2023.csv')
 
-def pegar_coluna (column_csv):
-    column = df[column_csv].tolist()
-    return column
-
-codigo_maquina = pegar_coluna('Maq_Cod')
-nome_maquina = pegar_coluna('Maq_nome')
-data_entrada = pegar_coluna('Dat_entr')
-ultima_manutencao = pegar_coluna('Last_maint')
-cep_localizacao_maq = pegar_coluna('Cep_locz_maq')
-
-BAIRRO_MAQUINA = 'https://h-apigateway.conectagov.estaleiro.serpro.gov.br/api-cep/v1/consulta/cep/'
-
-def get_bairro (cep):
-    response = requests.get(f'{BAIRRO_MAQUINA}/{cep}', timeout=5000)
-    return response.json() if response.status_code == 200 else None
-
-maquinas = [maquina for cod in codigo_maquina]
+# conversão do df em lista
+maquinas = df.values.tolist()
 print(json.dumps(maquinas, indent=2))
+
+# codigo_maquina = pegar_coluna('Maq_Cod')
+# nome_maquina = pegar_coluna('Maq_nome')
+# data_entrada = pegar_coluna('Dat_entr')
+# ultima_manutencao = pegar_coluna('Last_maint')
+# cep_localizacao_maq = pegar_coluna('Cep_locz_maq')
+
+# BAIRRO_MAQUINA = 
+# 'https://h-apigateway.conectagov.estaleiro.serpro.gov.br/api-cep/v1/consulta/cep/'
+
+# acesso a api dos correios
+# def get_bairro (cep):
+#     response = requests.get(f'{BAIRRO_MAQUINA}/{cep}', timeout=5000)
+#     return response.json() if response.status_code == 200 else None
